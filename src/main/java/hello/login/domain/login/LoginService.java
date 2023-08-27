3d041207ba4d77fd5bbeb89e5c69e993c70e81d2
seungsoo <seung_soos@naver.com>
2023-08-27 15:11:34 +0900

    - section 6 로그인 , 세션, 쿠키

---
+++ src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,22 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password){
+        Optional<Member> findMember = memberRepository.findByLoginId(loginId);
+
+        return findMember.filter(member -> member.getPassword().equals(password))
+                .orElse(null);
+    }
+}
